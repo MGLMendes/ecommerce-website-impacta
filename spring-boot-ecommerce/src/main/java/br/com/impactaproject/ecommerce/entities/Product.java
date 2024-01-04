6fe54ae6395a9a70f1b9026d5dcb1bfa5e1a4814
MGLMendes <miguelmendes203@gmail.com>
2024-01-04 13:51:03 -0300

    feat: Product Entity

---
+++ spring-boot-ecommerce/src/main/java/br/com/impactaproject/ecommerce/entities/Product.java
@@ -0,0 +1,31 @@
+package br.com.impactaproject.ecommerce.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "product")
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String sku;
+    private String name;
+    private String description;
+    private BigDecimal unitPrice;
+    private String imageUrl;
+    private Boolean active;
+    private Integer unitsInStock;
+    private Date dateCreated;
+    private Date lastUpdated;
+}
