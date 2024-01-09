43434e3676604450863497b5e02b83ba6fc7ca0b
MGLMendes <miguelmendes203@gmail.com>
2024-01-09 09:02:06 -0300

    removendo spring data rest

--- spring-boot-ecommerce/src/main/java/br/com/impactaproject/ecommerce/repositories/ProductCategoryRepository.java
+++ spring-boot-ecommerce/src/main/java/br/com/impactaproject/ecommerce/repositories/ProductCategoryRepository.java
@@ -2,8 +2,8 @@ package br.com.impactaproject.ecommerce.repositories;
 
 import br.com.impactaproject.ecommerce.entities.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+@Repository
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
