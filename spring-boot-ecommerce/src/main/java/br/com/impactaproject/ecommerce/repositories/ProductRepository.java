e152cbd1abf683bbb05f10ab689de351cfa412f8
mgLord <miguelmendes203@gmail.com>
2024-03-20 14:39:47 -0300

    implementando chamado de produtos por categoria

--- spring-boot-ecommerce/src/main/java/br/com/impactaproject/ecommerce/repositories/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/br/com/impactaproject/ecommerce/repositories/ProductRepository.java
@@ -1,10 +1,14 @@
 package br.com.impactaproject.ecommerce.repositories;
 
 import br.com.impactaproject.ecommerce.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(Long categoryId, Pageable pageable);
 }
