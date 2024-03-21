c600fdc7367a0473c1081cda94521c4a1a940266
MGLMendes <miguelmendes203@gmail.com>
2024-03-21 12:15:28 -0300

    fix: Ajsute da chamada dos produtos por categoria

--- frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -23,20 +23,18 @@ export class ProductListComponent implements OnInit {
   }
 
   listProducts() {
-    // check if "id" parameter is avaliable
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
     if (hasCategoryId) {
-      // get the "id" param string. convert string to a number using the "+" symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      this.currentCategoryId = 1
     }
-    else{
-      // not category id available ... default to category id 1
-      this.currentCategoryId = 1;
-    }
-    // now get the products for the given category id
+
+    console.log("Current ID:", this.currentCategoryId)
     this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
+        console.log("DATA: ", data)
         this.products = data;
       }
     )
