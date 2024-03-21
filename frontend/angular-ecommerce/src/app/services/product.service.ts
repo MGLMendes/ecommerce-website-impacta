4b077caa7bb934595c4c130e3c18424a7ada7c46
MGLMendes <miguelmendes203@gmail.com>
2024-03-21 12:46:09 -0300

    feat: Procurando dinâmicamente as categories

--- frontend/angular-ecommerce/src/app/services/product.service.ts
+++ frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -2,28 +2,40 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private baseUrl = "http://localhost:8080/api/products"
+  private baseUrl = "http://localhost:8080/api"
 
   constructor(private httpClient: HttpClient) { }
 
 
   getProductList(theCategoryId: number): Observable<Product[]> {
 
-    const searchUrl = `${this.baseUrl}/category/${theCategoryId}`;
+    const searchUrl = `${this.baseUrl}/products/category/${theCategoryId}`;
     console.log("Search URL: ", searchUrl)
-      return this.httpClient.get<GetResponse>(searchUrl).pipe(
+      return this.httpClient.get<GetProductsResponse>(searchUrl).pipe(
         map(response => response.content)
       )
+  }
+
+  getProductCategories(): Observable<ProductCategory[]>  {
+    const searchUrl = `${this.baseUrl}/products-category`
+    return this.httpClient.get<GetProductsCategoriesResponse>(searchUrl).pipe(
+      map(response => response.content)
+    )
   }
 }
 
 
-interface GetResponse {
+interface GetProductsResponse {
   content: Product[]
+}
+
+interface GetProductsCategoriesResponse {
+  content: ProductCategory[]
 }
